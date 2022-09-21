14e9f267b7c818250741cfa1d68da70b059c1eba
Maria Ines Parnisari <maineparnisari@gmail.com>
2022-09-21 12:19:15 -0700

    progress?

--- src/friendly-to-api.ts
+++ src/friendly-to-api.ts
@@ -57,99 +57,98 @@ const filterBlanksAndTrim = (list: string[]) => list.filter((entry) => entry).ma
 
 export const friendlySyntaxToApiSyntax = (config: string): TypeDefinitions => {
   const typeDefinitions: TypeDefinitions = { type_definitions: [] };
-  const result: string[][][][] = parseDSL(config);
+  const allTypeDefinitions: string[][][][] = parseDSL(config);
 
-  result.forEach((r) => {
-    const typeDef: TypeDefinition = { type: flattenDeep(r[0][2]).join(""), relations: {} };
-    const definitions = flatten(r[2]);
+  allTypeDefinitions.forEach((typeDefinition) => {
+    // typeDefinition[0] contains the first line "type xxx"
+    const nameOfType = flattenDeep(typeDefinition[0][2]).join("");
+    const typeDef: TypeDefinition = { type: nameOfType, relations: {} };
 
-    definitions.forEach((def) => {
-      const flattened: any = flatten(def);
-      const definition = flattenDeep(flatten(flattened[1] as [])
-        .filter((r: any) => flattenDeep(r).join("").trim().length)[1]).join("");
-      const relations: any[] = [];
-      let rawRelations: any[] = [];
+    // typeDefinition[1] contains "relations \n define yyy as zzz"
+    if (typeDefinition[1].length == 1) {
+      const relationDefinitions: any[] = flatten(typeDefinition[1][0][1]);
+      relationDefinitions.forEach((relationDefinition) => {
+        const flattenedRelationDefinition: any = flatten(relationDefinition);
+        const nameOfRelation = flattenedRelationDefinition[1][0][1].join("")
+        const relations: any[] = [];
+        let rawRelations: any[] = [...flatten(flattenedRelationDefinition[4])];
 
-      if (!!flattened[1]) { // at least one relation defined
-        rawRelations = [...flatten(flattened[1][4])];
-
-        if (flattened[1].length === 2) {
-          flattened[1][0][4] = flattened[1][0][4].concat(flatMapDeep(flattened[1][1]).join(""));
-          rawRelations = flattened[1][0][4];
+        // if (flattenedRelationDefinition[1].length === 2) {
+        //   flattenedRelationDefinition[1][0][4] = flattenedRelationDefinition[1][0][4].concat(flatMapDeep(flattenedRelationDefinition[1][1]).join(""));
+        //   rawRelations = flattenedRelationDefinition[1][0][4];
+        // }
+        for (const rawRelation of rawRelations) {
+          const flat = flattenDeep(rawRelation);
+          const value = flat.join("");
+  
+          if (isEmpty(flat)) {
+            break;
+          }
+  
+          if (typeof value === "string") {
+            relations.push(value);
+          } else {
+            relations.push(...(value as any));
+          }
         }
-      }    
-
-      for (const rawRelation of rawRelations) {
-        const flat = flattenDeep(rawRelation);
-        const value = flat.join("");
-
-        if (isEmpty(flat)) {
-          break;
-        }
-
-        if (typeof value === "string") {
-          relations.push(value);
+  
+        if (relations.length === 1) {
+          typeDef.relations[nameOfRelation] = resolveRelation(relations[0]);
         } else {
-          relations.push(...(value as any));
-        }
-      }
-
-      if (relations.length === 1) {
-        typeDef.relations[definition] = resolveRelation(relations[0]);
-      } else {
-        const isOr = flattenDeep(relations).join("").includes(` ${Keywords.OR} `);
-        const isAnd = flattenDeep(relations).join("").includes(` ${Keywords.AND} `);
-        const isButNot = flattenDeep(relations).join("").includes(` ${Keywords.BUT_NOT} `);
-        const isFrom = flattenDeep(relations).join("").includes(` ${Keywords.FROM} `);
-
-        if (isFrom) {
-          typeDef.relations[definition] = resolveRelation(flattenDeep(relations).join(""));
-        }
-
-        if (isOr) {
-          const child: Userset[] = [];
-          const list = [relations[0], ...relations[1].replace("or ", "").split(/\sor\s/)];
-
-          filterKeywords(filterBlanksAndTrim(list)).forEach((relation) => {
-            child.push(resolveRelation(relation));
-          });
-
-          typeDef.relations[definition] = {
-            union: {
-              child,
-            },
-          };
-        }
-
-        if (isAnd) {
-          const child: Userset[] = [];
-          const list = [relations[0], ...relations[1].replace("and ", "").split(/\sand\s/)];
-
-          filterKeywords(filterBlanksAndTrim(list)).forEach((relation) => {
-            child.push(resolveRelation(relation));
-          });
-
-          typeDef.relations[definition] = {
-            intersection: {
-              child,
-            },
-          };
-        }
-
-        if (isButNot) {
-          typeDef.relations[definition] = {
-            difference: {
-              base: {
-                ...resolveRelation(relations[0].trim()),
+          const isOr = flattenDeep(relations).join("").includes(` ${Keywords.OR} `);
+          const isAnd = flattenDeep(relations).join("").includes(` ${Keywords.AND} `);
+          const isButNot = flattenDeep(relations).join("").includes(` ${Keywords.BUT_NOT} `);
+          const isFrom = flattenDeep(relations).join("").includes(` ${Keywords.FROM} `);
+  
+          if (isFrom) {
+            typeDef.relations[nameOfRelation] = resolveRelation(flattenDeep(relations).join(""));
+          }
+  
+          if (isOr) {
+            const child: Userset[] = [];
+            const list = [relations[0], ...relations[1].replace("or ", "").split(/\sor\s/)];
+  
+            filterKeywords(filterBlanksAndTrim(list)).forEach((relation) => {
+              child.push(resolveRelation(relation));
+            });
+  
+            typeDef.relations[nameOfRelation] = {
+              union: {
+                child,
               },
-              subtract: {
-                ...resolveRelation(relations[1].split("but not ")[1].trim()),
+            };
+          }
+  
+          if (isAnd) {
+            const child: Userset[] = [];
+            const list = [relations[0], ...relations[1].replace("and ", "").split(/\sand\s/)];
+  
+            filterKeywords(filterBlanksAndTrim(list)).forEach((relation) => {
+              child.push(resolveRelation(relation));
+            });
+  
+            typeDef.relations[nameOfRelation] = {
+              intersection: {
+                child,
               },
-            },
-          };
+            };
+          }
+  
+          if (isButNot) {
+            typeDef.relations[nameOfRelation] = {
+              difference: {
+                base: {
+                  ...resolveRelation(relations[0].trim()),
+                },
+                subtract: {
+                  ...resolveRelation(relations[1].split("but not ")[1].trim()),
+                },
+              },
+            };
+          }
         }
-      }
-    });
+      });
+    }
 
     typeDefinitions.type_definitions!.push(typeDef);
   });
