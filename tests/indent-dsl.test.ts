6b1b1bfc7a87bbacb5d5cb7a754af032928782fa
David Barrat <david@barrat.io>
2022-10-15 01:03:36 +0200

    feat: add indentDSL to improve checkDSL parsing reliability (#68)
    * feat: add indentDSL to improve checkDSL parsing reliability

---
+++ tests/indent-dsl.test.ts
@@ -0,0 +1,62 @@
+import { friendlySyntaxToApiSyntax, indentDSL } from "../src";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { testModels } from "./data";
+
+const getRandomNumber = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const generateRandomNumberOfSpaces = () => {
+  return " ".repeat(getRandomNumber(0, 30));
+};
+
+const generateRandomNewLines = () => {
+  return "\n".repeat(getRandomNumber(0, 15));
+};
+
+describe("indent-dsl", () => {
+  testModels.forEach((testCase) => {
+    it(`should indent ${testCase.name}`, () => {
+      const rawFriendly = testCase.friendly
+        .split("\n")
+        .map((line) => generateRandomNumberOfSpaces() + line.trim())
+        .join("\n");
+      const apiSyntax = friendlySyntaxToApiSyntax(indentDSL(rawFriendly));
+      expect(apiSyntax).toEqual(testCase.json);
+    });
+  });
+
+  testModels.forEach((testCase) => {
+    it(`should indent ${testCase.name} with random spaces and empty lines removed`, () => {
+      const rawFriendly = testCase.friendly
+        .split("\n")
+        .map((line) => generateRandomNumberOfSpaces() + line.trim() + generateRandomNewLines())
+        .join("\n");
+      const apiSyntax = friendlySyntaxToApiSyntax(indentDSL(rawFriendly, true));
+      expect(apiSyntax).toEqual(testCase.json);
+    });
+  });
+
+  testModels.forEach((testCase) => {
+    it(`should indent ${testCase.name} with empty lines removed`, () => {
+      const rawFriendly = testCase.friendly
+        .split("\n")
+        .map((line) => line.trim() + generateRandomNewLines())
+        .join("\n");
+      const apiSyntax = friendlySyntaxToApiSyntax(indentDSL(rawFriendly, true));
+      expect(apiSyntax).toEqual(testCase.json);
+    });
+  });
+
+  testModels.forEach((testCase) => {
+    it(`should indent ${testCase.name} with empty lines not removed`, () => {
+      const rawFriendly = testCase.friendly
+        .split("\n")
+        .map((line) => line.trim() + generateRandomNewLines())
+        .join("\n");
+      const apiSyntax = friendlySyntaxToApiSyntax(indentDSL(rawFriendly, false));
+      expect(apiSyntax).toEqual(testCase.json);
+    });
+  });
+});
