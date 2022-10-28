d7ae258c93bcd1977cdd526293834b644c107756
Adrian Tam <adrian.tam@auth0.com>
2022-10-28 12:04:33 -0400

    fix: correct error location in model 1.0
    Close https://github.com/openfga/syntax-transformer/issues/82

--- tests/data/model-validation.ts
+++ tests/data/model-validation.ts
@@ -135,4 +135,32 @@ type org
       },
     ],
   },
+  {
+    name: "ensure errors are highlighted in the right place for same relation name across different types",
+    friendly: `type user
+type geography
+  relations
+    define parent as self
+    define can_view_locations as self or can_view_locations from parent
+
+type outlet
+  relations
+    define geography as self
+    define can_view_locations as self or can_view_locations from parent
+`,
+    expectedError: [
+      {
+        endColumn: 72,
+        endLineNumber: 10,
+        message: "The relation `parent` does not exist in type `outlet`",
+        relatedInformation: {
+          type: "",
+        },
+        severity: 8,
+        source: "linter",
+        startColumn: 66,
+        startLineNumber: 10,
+      },
+    ],
+  },
 ];
