3c31ce9aa2317b06b3fe9786a94a0d1db5d04ef4
Daniel Jeffery <danielajeffery@gmail.com>
2024-04-05 10:10:13 -0700

    fix: correct error ranges

--- src/tools/monaco/validate.ts
+++ src/tools/monaco/validate.ts
@@ -1,20 +1,15 @@
 import type * as MonacoEditor from "monaco-editor";
 
 import { Marker } from "./typings";
-import {
-  DSLSyntaxError,
-  DSLSyntaxSingleError,
-  ModelValidationError,
-  ModelValidationSingleError,
-} from "@openfga/syntax-transformer/dist/errors";
-import { validator } from "@openfga/syntax-transformer";
+import { DSLSyntaxSingleError, ModelValidationSingleError } from "@openfga/syntax-transformer/dist/errors";
+import { errors, validator } from "@openfga/syntax-transformer";
 
 export function validateDSL(monaco: typeof MonacoEditor, dsl: string): Marker[] {
   const markers: Marker[] = [];
   try {
     validator.validateDSL(dsl);
   } catch (err) {
-    for (const singleErr of (err as DSLSyntaxError | ModelValidationError).errors) {
+    for (const singleErr of (err as errors.BaseMultiError<errors.BaseError>).errors) {
       let source;
 
       if (singleErr instanceof DSLSyntaxSingleError) {
@@ -41,10 +36,10 @@ export function validateDSL(monaco: typeof MonacoEditor, dsl: string): Marker[] 
       markers.push({
         message: singleErr.msg,
         severity: monaco.MarkerSeverity.Error,
-        startColumn: singleErr?.column?.start || 0,
-        endColumn: singleErr?.column?.end || 0,
-        startLineNumber: singleErr?.line?.start || 0,
-        endLineNumber: singleErr?.line?.end || 0,
+        startColumn: (singleErr?.column?.start || 0) + 1,
+        endColumn: (singleErr?.column?.end || 0) + 1,
+        startLineNumber: (singleErr?.line?.start || 0) + 1,
+        endLineNumber: (singleErr?.line?.end || 0) + 1,
         source,
         extraInformation,
       });
