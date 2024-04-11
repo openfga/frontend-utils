10f07f18bbd35fea624df4abd879e4ef3ffb0b1e
Daniel Jeffery <1425457+d-jeffery@users.noreply.github.com>
2024-04-10 19:42:50 -0700

    feat: modular models support for prism (#238)
    feat: modular models support

--- src/tools/prism/language-definition.ts
+++ src/tools/prism/language-definition.ts
@@ -1,25 +1,26 @@
 import { OpenFgaDslThemeTokenType } from "../../theme/theme.typings";
 
 export const languageDefinition = {
-  [OpenFgaDslThemeTokenType.COMMENT]: {
-    pattern: /^\s*#.*/,
+  [OpenFgaDslThemeTokenType.MODULE]: {
+    pattern: /(module\s+)[\w_-]+/i,
+    lookbehind: true,
   },
-  [OpenFgaDslThemeTokenType.KEYWORD]: /\b(type|relations|define|and|or|but not|from|as|model|schema|condition)\b/,
   [OpenFgaDslThemeTokenType.TYPE]: {
     pattern: /(\btype\s+)\w+/i,
     lookbehind: true,
-    greedy: true,
+  },
+  [OpenFgaDslThemeTokenType.EXTEND]: {
+    pattern: /(\bextend type\s+)\w+/i,
+    lookbehind: true,
   },
   [OpenFgaDslThemeTokenType.RELATION]: {
     pattern: /(\bdefine\s+)\w+/i,
     lookbehind: true,
-    greedy: true,
   },
   [OpenFgaDslThemeTokenType.DIRECTLY_ASSIGNABLE]: /\[.*]|self/,
   [OpenFgaDslThemeTokenType.CONDITION]: {
     pattern: /(\bcondition\s+)\w+/i,
     lookbehind: true,
-    greedy: true,
   },
   "condition-params": {
     pattern: /\(.*\)\s*{/,
@@ -27,5 +28,11 @@ export const languageDefinition = {
       "condition-param": /\b(\w+)\s*:/i,
       "condition-param-type": /\b(string|int|map|uint|list|timestamp|bool|duration|double|ipaddress)\b/,
     },
+  },
+  [OpenFgaDslThemeTokenType.COMMENT]: {
+    pattern: /(^\s*|\s+)#.*/,
+  },
+  [OpenFgaDslThemeTokenType.KEYWORD]: {
+    pattern: /\b(type|relations|define|and|or|but not|from|as|model|schema|condition|module|extend)\b/,
   },
 };
